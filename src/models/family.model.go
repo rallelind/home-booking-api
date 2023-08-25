77e63223a6fc6facf3f1f880b442efe187195216
Rasmus Lind <rasmus@voiceboxer.com>
2023-08-25 19:20:22 +0200

    fixing

--- src/models/family.model.go
+++ src/models/family.model.go
@@ -1,11 +1,13 @@
 package models
 
-import "github.com/lib/pq"
+import (
+	"github.com/lib/pq"
+)
 
 type FamilyModel struct {
 	Id         int            `db:"id" json:"id"`
 	FamilyName string         `db:"family_name" json:"family_name"`
 	Members    pq.StringArray `db:"members" json:"members"`
 	HouseId    int            `db:"house_id" json:"house_id"`
-	CoverImage string         `db:"cover_image" json:"cover_image"`
+	CoverImage *string        `db:"cover_image" json:"cover_image"`
 }
